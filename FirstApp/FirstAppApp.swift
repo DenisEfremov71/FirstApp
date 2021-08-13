f201473998a28f7061b4ce2523a620b27319b0dd
Denis Efremov <denis.efremov@freightera.com>
2021-08-13 15:31:05 -0700

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Denis Efremov on 2021-08-13.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
